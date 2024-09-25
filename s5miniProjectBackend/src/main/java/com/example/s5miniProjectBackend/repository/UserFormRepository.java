d85d10ec98001564404fce69f2863abb9b826e26
muhammedArsat <ars21.code@gmail.com>
2024-09-25 15:40:47 +0530

    added feature to show only pending List in Admin Approve Page

--- s5miniProjectBackend/src/main/java/com/example/s5miniProjectBackend/repository/UserFormRepository.java
+++ s5miniProjectBackend/src/main/java/com/example/s5miniProjectBackend/repository/UserFormRepository.java
@@ -2,8 +2,13 @@ package com.example.s5miniProjectBackend.repository;
 
 import com.example.s5miniProjectBackend.entity.UserForm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserFormRepository extends JpaRepository<UserForm, Integer> {
+
+    List<UserForm> findByStatus(String pending);
 }
